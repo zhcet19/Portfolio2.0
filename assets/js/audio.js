c72cb0aa8dc47678ab2ee8d6ef065a4048e58a7e
zhcet19 <faizamu19@gmail.com>
2022-07-23 12:59:39 +0530

    added minor tweaks

--- assets/js/audio.js
+++ assets/js/audio.js
@@ -3,3 +3,14 @@ function setHalfVolume() {
   vid.volume = 0.15;
 } 
 setHalfVolume();
+
+
+const checkbox1 = document.getElementById('checkbox-1');
+
+checkbox1.addEventListener('change', ()=>{
+
+  var audio=document.getElementById("audiosrc");
+audio.src = "../audio/music2.mp3";
+  
+   
+})
